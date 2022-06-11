4554cf9442e754479a352dc18a40b3a9d8e1ec86
Sergey Onufrienko <sergey@onufrienko.com>
2022-06-11 21:49:31 +0300

    Invoice Rust commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,28 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn do_command() {
+    println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn post_command(user_name: String) {
+    println!("I was invoked from JS, with this message: {}", user_name);
+}
+
+#[tauri::command]
+fn get_command(user_name: String) -> String {
+    format!("Hello {}", user_name)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![do_command])
+        .invoke_handler(tauri::generate_handler![post_command])
+        .invoke_handler(tauri::generate_handler![get_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
+  const clickHandler = async () => {
+    // Invoke the command
+    invoke('do_command');
+    invoke('post_command', { userName: 'Sergii' });
+    const res = await invoke('get_command', { userName: 'Sergii' });
+    alert(res);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            Checkout
+          </Typography>
+          <p>Hello</p>
+          <Button variant="contained" onClick={clickHandler}>
+            Hello World
+          </Button>
+        </Paper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
